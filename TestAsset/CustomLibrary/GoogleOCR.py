dcb4429442734cb621832513f02bee620b94cac8
hemendra0851 <43489915+hemendra0851@users.noreply.github.com>
2020-02-13 23:19:30 +0530

    Create GoogleOCR.py

---
+++ TestAsset/CustomLibrary/GoogleOCR.py
@@ -0,0 +1,10 @@
+from PIL import Image
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+im = Image.open("C:\\Users\\Hemendra Pratap\\Downloads\\download.jpg")
+
+text = pytesseract.image_to_string(im, lang = 'eng')
+
+print(text)
